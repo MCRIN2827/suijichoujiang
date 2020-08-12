afaefb302655a3efae9e9599d6ba537a444c4e91
MCRIN2827 <rinmc1037@gmail.com>
2020-08-12 10:16:13 +0800

    beautify

--- 随机抽奖.py
+++ 随机抽奖.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from random import randint
-All = raw_input('请输入参与抽奖总人数:')
-Num = raw_input('请输入需要抽奖个数:')
+All_People = raw_input('请输入参与抽奖总人数:')
+Luckydogs = raw_input('请输入需要抽奖个数:')
 flag = True
 
 while flag:
-    if Num.isdigit() and All.isdigit():
-        Ind1 = int(Num)
-        Ind2 = int(All)
+    if Luckydogs.isdigit() and All_People.isdigit():
+        Num_Luckydogs = int(Luckydogs)
+        Num_All_People = int(All_People)
         flag = False
     else:
         print '输入错误'
         break
 else:
-    for i in range(Ind1):
-        print randint(1,Ind2)    #呵呵
+    for i in range( Num_Luckydogs):
+        print randint(1,Num_All_People)    #呵呵
